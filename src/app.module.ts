e96c0b404467e8687e53465807c9259994229e49
romit-gitcode <romitp.1401@gmail.com>
2023-03-28 10:36:01 +0530

    authentication added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,12 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthMiddleware } from './middleware/auth.middleware';
 import { AuthModule } from './modules/auth/auth.module';
 import { TodoModule } from './modules/todo/todo.module';
 import { SharedModule } from './shared/shared.module';
@@ -10,4 +16,14 @@ import { SharedModule } from './shared/shared.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes(
+      {
+        path: 'todo',
+        method: RequestMethod.POST,
+      },
+      { path: 'todo/:id', method: RequestMethod.DELETE },
+    );
+  }
+}
--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -8,14 +8,14 @@ import { NextFunction } from 'express';
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    let authorization;
+    let authorization: any;
     if ((authorization = req.headers['authorization'])) {
       let basic = authorization.split(' ');
       console.log(basic);
       if (basic) {
         basic = basic[1];
         basic = Buffer.from(basic, 'base64').toString('utf8');
-        if (basic == 'vishal:123456') {
+        if (basic == 'romit:123456') {
           return next();
         }
       }
